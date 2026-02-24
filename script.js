95db390ceb0d255dbdb53313cd7b61be899aa780
kasperab <kasperab@yahoo.se>
2026-02-24 16:02:39 +0100

    Added support for touch

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 let drawing = false;
+context.lineWidth = 8;
 
 canvas.addEventListener("mousedown", event => {
-	drawing = true;
-	context.beginPath();
-	const position = getPosition(event.clientX, event.clientY);
-	context.moveTo(position.x, position.y);
-	context.clearRect(0, 0, canvas.width, canvas.height);
+	startDrawing(getPosition(event));
+});
+canvas.addEventListener("touchstart", event => {
+	startDrawing(getPosition(event.touches[0]));
 });
 
 canvas.addEventListener("mouseup", () => drawing = false);
+canvas.addEventListener("touchend", () => drawing = false);
 
 canvas.addEventListener("mousemove", event => {
 	if (drawing) {
-		const position = getPosition(event.clientX, event.clientY);
-		context.lineTo(position.x, position.y);
-		context.stroke();
+		draw(getPosition(event));
+	}
+});
+canvas.addEventListener("touchmove", event => {
+	if (drawing) {
+		event.preventDefault();
+		event.stopPropagation();
+		draw(getPosition(event.touches[0]));
 	}
 });
 
-function getPosition(inX, inY) {
+function startDrawing(position) {
+	drawing = true;
+	context.beginPath();
+	context.moveTo(position.x, position.y);
+	context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function draw(position) {
+	context.lineTo(position.x, position.y);
+	context.stroke();
+}
+
+function getPosition(event) {
 	const scale = canvas.width / canvas.clientWidth;
-	const x = (inX - canvas.offsetLeft) * scale;
-	const y = (inY - canvas.offsetTop) * scale;
+	const x = (event.clientX - canvas.offsetLeft) * scale;
+	const y = (event.clientY - canvas.offsetTop) * scale;
 	return {x, y};
 }
