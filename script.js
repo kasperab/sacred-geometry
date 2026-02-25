ac38905300c1faf887dba8775646d178401c0457
kasperab <kasperab@yahoo.se>
2026-02-25 12:31:26 +0100

    Constrained drawing to a circle

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+
+const center = { x: canvas.width / 2, y: canvas.height / 2 };
+const radius = canvas.width / 2;
+const drawWidth = 8;
+const borderWidth = 4;
 let drawing = false;
-context.lineWidth = 8;
 
 canvas.addEventListener("mousedown", event => {
 	startDrawing(getPosition(event));
@@ -10,8 +14,8 @@ canvas.addEventListener("touchstart", event => {
 	startDrawing(getPosition(event.touches[0]));
 });
 
-canvas.addEventListener("mouseup", () => drawing = false);
-canvas.addEventListener("touchend", () => drawing = false);
+document.addEventListener("mouseup", () => drawing = false);
+document.addEventListener("touchend", () => drawing = false);
 
 canvas.addEventListener("mousemove", event => {
 	if (drawing) {
@@ -26,14 +30,33 @@ canvas.addEventListener("touchmove", event => {
 	}
 });
 
+drawBorder();
+
 function startDrawing(position) {
+	if (!inCircle(position)) {
+		return;
+	}
 	drawing = true;
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	drawBorder();
+	context.lineWidth = drawWidth;
 	context.beginPath();
 	context.moveTo(position.x, position.y);
-	context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawBorder() {
+	context.lineWidth = borderWidth;
+	context.beginPath();
+	context.arc(center.x, center.y, radius - borderWidth / 2, 0, Math.PI * 2);
+	context.moveTo(center.x, center.y);
+	context.arc(center.x, center.y, borderWidth / 2, 0, Math.PI * 2);
+	context.stroke();
 }
 
 function draw(position) {
+	if (!inCircle(position)) {
+		return;
+	}
 	context.lineTo(position.x, position.y);
 	context.stroke();
 }
@@ -42,5 +65,9 @@ function getPosition(event) {
 	const scale = canvas.width / canvas.clientWidth;
 	const x = (event.clientX - canvas.offsetLeft) * scale;
 	const y = (event.clientY - canvas.offsetTop) * scale;
-	return {x, y};
+	return { x, y };
+}
+
+function inCircle(position) {
+	return Math.sqrt(Math.pow(center.x - position.x, 2) + Math.pow(center.y - position.y, 2)) < radius;
 }
